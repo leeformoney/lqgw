163f394406cc1963aa2f42847cb95f7dd3835cbd
kichen <84363330+leeformoney@users.noreply.github.com>
2025-05-13 19:16:43 +0800

    Add files via upload

---
+++ config/oss-config.js
@@ -0,0 +1,8 @@
+// 本地配置OSS信息（仅限开发测试使用）
+export const ossConfig = {
+  region: 'oss-cn-hangzhou',       // 您的OSS区域
+  accessKeyId: 'LTAI5tFxffoCWU6RuvK3SHzX',  // 您的AccessKey ID
+  accessKeySecret: 'MDBwyATBQAjME8zPhaRWSLgwRFSHXB',  // 您的AccessKey Secret（有安全风险！）
+  bucket: 'lqdx1',      // 您的Bucket名称
+  secure: true                     // 使用HTTPS
+}
